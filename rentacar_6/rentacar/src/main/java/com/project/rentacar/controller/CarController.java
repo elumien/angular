129b5c8528c95ea2e93ab75deba7c1ce16e53321
Fellner Sára <sara.fellner@loxon.eu>
2018-09-18 17:47:52 +0200

    form #3

--- rentacar_6/rentacar/src/main/java/com/project/rentacar/controller/CarController.java
+++ rentacar_6/rentacar/src/main/java/com/project/rentacar/controller/CarController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class CarController {
 
@@ -46,4 +48,6 @@ public class CarController {
             return returnPage;
 
     }
+
+
 }
